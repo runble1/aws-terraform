2c698b40883dfc0b71d8a13356c3f8a64f629d35
runble1 <runble1@gmail.com>
2023-11-04 13:30:44 +0900

    Add AWS PROXY

--- aws_cognito/app2/src/index.ts
+++ aws_cognito/app2/src/index.ts
@@ -10,22 +10,40 @@ const docClient = new DynamoDB.DocumentClient();
 export const handler = (event: any, context: Context, callback: Callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
+    const artist = event.artist ? event.artist : (event.queryStringParameters ? event.queryStringParameters.artist : null);
+
+    if (!artist) {
+        callback(null, {
+            statusCode: 400,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: 'Artist parameter is missing' })
+        });
+        return;
+    }
+
     const params = {
-        // TODO: 作成したDynamoDBテーブルの名前に書き換えてください
         TableName: DYNAMODB_TABLE_NAME,
         KeyConditionExpression: 'Artist = :artist',
         ExpressionAttributeValues: {
-            ':artist': event.artist
+            ':artist': artist
         }
     };
 
     docClient.query(params, (err, data) => {
         if (err) {
             console.log('Error', err);
-            callback(Error(err.message));
+            callback(null, {
+                statusCode: 500,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ error: err.message })
+            });
         } else {
             console.log('Success', data);
-            callback(null, data.Items);
+            callback(null, {
+                statusCode: 200,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data.Items)
+            });
         }
     });
 };
