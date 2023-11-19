087389b69331e0b176dc7ac1eaf9715de6ba61b1
runble1 <runble1@gmail.com>
2023-11-19 13:25:38 +0900

    cloudfrontからnext.js

--- aws_nextjs_apigateway_cloudfront/envs/dev/output.tf
+++ aws_nextjs_apigateway_cloudfront/envs/dev/output.tf
@@ -10,8 +10,7 @@ output "endpoint_url" {
   value = module.api_gateway.endpoint_url
 }
 
-/*
 output "cloudfront_distribution_url" {
   value       = module.cloudfront.cloudfront_distribution_url
   description = "The URL of the CloudFront distribution"
-}*/
+}
