88a152b1fa3bdd0fee90b587fb956ac65ec4040d
runble1 <runble1@gmail.com>
2024-01-08 09:53:28 +0900

    add

---
+++ aws_lambda_cloudwatch/env/dev/main.tf
@@ -0,0 +1,15 @@
+locals {
+  service = "lambda-cwl"
+}
+
+module "cloudwatch" {
+  source            = "../../modules/cloudwatch"
+  function_name     = "${var.env}-${local.service}"
+  service           = "${var.env}-${local.service}"
+  log_group_name    = "/aws/lambda/${var.env}-${local.service}"
+}
+
+module "lambda" {
+  source        = "../../modules/lambda"
+  function_name = "${var.env}-${local.service}"
+}
---
+++ aws_lambda_cloudwatch/modules/cloudwatch/logs.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = var.log_group_name
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_group" "kinesis_error" {
+  name = "/aws/kinesisfirehose/${var.function_name}-log-stream"
+  retention_in_days = 30
+}
