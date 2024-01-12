fdec461413f35802615f53667e3bfea1ff488833
runble1 <runble1@gmail.com>
2024-01-13 08:47:17 +0900

    add

--- aws_lambda/modules/lambda/kms.tf
+++ aws_lambda/modules/lambda/kms.tf
@@ -1,13 +1,9 @@
 resource "aws_kms_key" "lambda_key" {
-  description             = "My Lambda Function Customer Master Key"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
+  description              = "My Lambda Function Managed Key"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  enable_key_rotation      = true
+  deletion_window_in_days  = 7
   tags = {
     Name = "${var.function_name}-lambda"
   }
-}
-
-resource "aws_kms_alias" "lambda_key_alias" {
-  name          = "alias/${var.function_name}"
-  target_key_id = aws_kms_key.lambda_key.id
 }
